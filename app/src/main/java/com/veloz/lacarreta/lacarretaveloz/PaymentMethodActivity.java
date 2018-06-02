319134e233011e3fac1d460f94756327efb9f2db
sebash <bastianhdezo@gmail.com>
2018-06-02 14:34:26 -0500

    hola

--- app/src/main/java/com/veloz/lacarreta/lacarretaveloz/PaymentMethodActivity.java
+++ app/src/main/java/com/veloz/lacarreta/lacarretaveloz/PaymentMethodActivity.java
@@ -1,18 +1,71 @@
 package com.veloz.lacarreta.lacarretaveloz;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class PaymentMethodActivity extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
+    ListView lista=null;
+    ArrayAdapter<String> adapter;
+    ArrayList <String> arraylist;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_method);
+
+        lista = (ListView)findViewById(R.id.milista);
+        arraylist = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arraylist);
+        lista.setAdapter(adapter);
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("pagos");
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String v1 = dataSnapshot.getValue(String.class);
+                arraylist.add(v1);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         /*setContentView(R.layout.activity_payment_method);
         ListView lista;
@@ -22,10 +75,7 @@ public class PaymentMethodActivity extends AppCompatActivity {
         lista.setAdapter(adaptador);*/
 
 
-        final ListView lista = (ListView)findViewById(R.id.milista);
-        String[] values = new String[]{"Ironman","Capitan America","Hulk","Thor","Black Widow","Ant man","Spider man"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
-        lista.setAdapter(adapter);
+
 
     }
 }
