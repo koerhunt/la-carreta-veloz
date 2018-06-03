4fad373232bb8ee7ebe2a228eda3f15dfa983a6d
Elias <eliasdevsanchez@gmail.com>
2018-06-02 20:01:49 -0500

    funciona perfil del usuario

--- app/src/main/java/com/veloz/lacarreta/lacarretaveloz/AccountActivity.java
+++ app/src/main/java/com/veloz/lacarreta/lacarretaveloz/AccountActivity.java
@@ -1,12 +1,12 @@
 package com.veloz.lacarreta.lacarretaveloz;
 
+import android.accounts.Account;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,15 +16,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 
 public class AccountActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
+
+
     TextView numcon;
     TextView nom;
     TextView car;
     TextView sem;
+
+    DatabaseReference mDatabase;
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
 
@@ -32,22 +34,17 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        ListView lista=null;
-        ArrayAdapter<String> adapter;
-        ArrayList<String> arraylist;
-
 
         numcon = (TextView)findViewById(R.id.nc);
         nom = (TextView)findViewById(R.id.n);
         car = (TextView)findViewById(R.id.c);
         sem = (TextView)findViewById(R.id.s);
 
-
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
 
-        //URL DE LA BASE DE DATOS
 
+        //URL DE LA BASE DE DATOS
         mDatabase = FirebaseDatabase.getInstance().getReference(("usuarios/"+currentUser.getUid().toString()));
 
 
@@ -56,24 +53,28 @@ public class AccountActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                DataSnapshot sn;
                 int count=0;
                 Iterator<DataSnapshot> iterador = dataSnapshot.getChildren().iterator();
+
+                Toast.makeText(AccountActivity.this,"INFORMACION RECIBIDA",Toast.LENGTH_SHORT).show();
 
                 while(iterador.hasNext()){
                     switch (count){
                         case 0: //carrera
-                                car.setText(iterador.next().getValue().toString());
+                            car.setText(iterador.next().getValue().toString());
                             break;
                         case 1: //nocontrol
-                                numcon.setText(iterador.next().getValue().toString());
+                            numcon.setText(iterador.next().getValue().toString());
                             break;
                         case 2: //nombre
-                                numcon.setText(iterador.next().getValue().toString());
+                            nom.setText(iterador.next().getValue().toString());
                             break;
                         case 3: //semestre
-                                sem.setText(iterador.next().getValue().toString());
+                            sem.setText(iterador.next().getValue().toString());
                             break;
+                        default:
+                            iterador.next();
+
                     }
                     count++;
                 }
@@ -83,10 +84,9 @@ public class AccountActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
-        });
 
+        });
 
     }
 }
--- app/src/main/java/com/veloz/lacarreta/lacarretaveloz/HomeActivity.kt
+++ app/src/main/java/com/veloz/lacarreta/lacarretaveloz/HomeActivity.kt
@@ -63,17 +63,14 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
 
             R.id.nav_cuenta -> {
-                mAuth?.signOut()
                 var intent  = Intent(applicationContext,AccountActivity::class.java)
                 startActivity(intent)
-                finish()
 
             }
             R.id.nav_signout -> {
                 mAuth?.signOut()
                 var intent  = Intent(applicationContext,WelcomeActivity::class.java)
                 startActivity(intent)
-                finish()
             }
 
         }
