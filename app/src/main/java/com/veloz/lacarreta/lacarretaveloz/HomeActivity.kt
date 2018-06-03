e324af648d3f8caf2ba0d056ba3fab700e61287b
Elias <eliasdevsanchez@gmail.com>
2018-06-03 15:07:56 -0500

    agregada llave de google maps

--- app/src/main/java/com/veloz/lacarreta/lacarretaveloz/HomeActivity.kt
+++ app/src/main/java/com/veloz/lacarreta/lacarretaveloz/HomeActivity.kt
@@ -11,12 +11,18 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar_home.*
 
-class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     //FIREBASE AUTH
     var mAuth: FirebaseAuth? = null
@@ -27,6 +33,9 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     var t1 : TextView? = null
     var t2 : TextView? = null
 
+    //mapa
+    private lateinit var mMap: GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -34,14 +43,18 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
 
+        //obtener al usuario logeado
         mAuth = FirebaseAuth.getInstance()
         current_user = mAuth?.currentUser
 
+        //accion del boton
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Obteniendo choferes, por favor espere...", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
 
+
+        //menu lateral
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
@@ -50,11 +63,10 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         nav_view.setNavigationItemSelectedListener(this)
 
-        t1 = findViewById(R.id.username)
-        t2 = findViewById(R.id.userphone)
-
-        t1?.text="dasdasdasdas"
-        t2?.text="618 123 12 23"
+        //inicializacion del mapa
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
     }
 
@@ -95,6 +107,17 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
 
+
+        // Add a marker in Sydney and move the camera
+        val itd = LatLng(24.032102,-104.646241)
+
+        mMap.addMarker(MarkerOptions().position(itd).title("ITD"))
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(itd,14f) )
+
+
+    }
 
 }
